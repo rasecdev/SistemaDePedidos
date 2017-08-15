6a7330dcc627c38ff7a197bf3a9bf4553089bca7
Anderson Pereira <anderson.rasec@outlook.com>
2017-08-14 23:39:12 -0300

    Criação da view de listagem dos produtos.

---
+++ Sistema/Context/LojaContext.cs
@@ -0,0 +1,11 @@
+﻿using Sistema.Models;
+using System.Data.Entity;
+
+namespace Sistema.Context
+{
+    public class LojaContext : DbContext
+    {
+        public DbSet<Produto> Produto { get; set; }
+        
+    }
+}
--- Sistema/Controllers/ProdutoController.cs
+++ Sistema/Controllers/ProdutoController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Sistema.Context;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,13 @@ namespace Sistema.Controllers
 {
     public class ProdutoController : Controller
     {
+
+        private LojaContext db = new LojaContext();
+
         // GET: Produto
         public ActionResult Index()
         {
-            return View();
+            return View(db.Produto.ToList());
         }
 
         // GET: Produto/Details/5
